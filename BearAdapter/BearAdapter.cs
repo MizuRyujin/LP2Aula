b0944afb85128320e91ca80f8067e99eea5845e9
Miguel Fernandez <miguelrf95@gmail.com>
2019-10-30 15:12:31 +0000

    Add BrownBear class and BearAdapter to allow bears to be treated as dogs

---
+++ BearAdapter/BearAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BearAdapter
+{
+    class BearAdapter : IDog
+    {
+        private IBear bear;
+
+        public void Bark()
+        {
+            bear.Roar();
+        }
+
+        public void Fetch(object objectToFetch)
+        {
+            bear.LookAt(objectToFetch);
+            bear.GoTowards(objectToFetch);
+            if (bear.TryEat(objectToFetch))
+            {
+                Console.WriteLine("Yeeeey, I ate the " + objectToFetch);
+            }
+            else
+            {
+                Console.WriteLine("Shit" + objectToFetch + "got away...");
+            }
+            
+        }
+
+        public BearAdapter(IBear bear)
+        {
+            this.bear = bear;
+        }
+    }
+}
---
+++ BearAdapter/BrownBear.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BearAdapter
+{
+    public class BrownBear : IBear
+    {
+        Random rnd;
+        public bool Hibernating { get; set; }
+
+        public void GoTowards(object objectToWalkTowards)
+        {
+            if (!Hibernating)
+            {
+                Console.WriteLine("Bear is moving towards " + objectToWalkTowards);
+            }
+        }
+
+        public void LookAt(object objectToLookAt)
+        {
+            if (!Hibernating)
+            {
+                Console.WriteLine("Looking at " + objectToLookAt);
+            }
+        }
+
+        public void Roar()
+        {
+            if (Hibernating)
+            {
+                Console.WriteLine("zzzZZZZzzzZZZ");
+            }
+            else
+            {
+                Console.WriteLine("Rooooooaaaar!!! xD");
+            }
+        }
+
+        public bool TryEat(object objectToEat)
+        {
+            if (!Hibernating && rnd.NextDouble() > 0.7)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public BrownBear(bool hibernating = false)
+        {
+            rnd = new Random();
+            hibernating = Hibernating;
+        }
+    }
+}
